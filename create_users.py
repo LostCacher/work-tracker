15b8618a0568d3500b8331baecc7c81f5f83b931
LostCacher <51054456+LostCacher@users.noreply.github.com>
2025-02-01 12:53:52 +0100

    Create create_users.py

---
+++ create_users.py
@@ -0,0 +1,16 @@
+from werkzeug.security import generate_password_hash
+
+from models import User, db
+from work_tracker import app
+
+with app.app_context():
+    db.create_all()
+    username = "username"
+    password = "password"
+    user = User(username=username)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+    print(f"User {username} created successfully!")
+
+bla
